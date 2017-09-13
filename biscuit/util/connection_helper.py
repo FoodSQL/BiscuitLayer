188031061ce498c61d67eac3fd5815343bb55abc
Rafael Ismael <Rafael Ismael>
2017-09-13 13:56:16 -0300

    Fixed db password

--- biscuit/util/connection_helper.py
+++ biscuit/util/connection_helper.py
@@ -47,7 +47,7 @@ class ConnectionHelper:
             conn_opt = {
                 'host': host,
                 'user': user,
-                'password': "Derrick1",
+                'password': password,
                 'database': database,
             }
             return conn_opt
